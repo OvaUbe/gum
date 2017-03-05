7e241d992c528052a0f5d679b61a89819394200d
OvaUbe <trevorismay@gmail.com>
2017-03-05 20:50:03 +0300

    Add lookup test.

--- test/flat_map_test.cpp
+++ test/flat_map_test.cpp
@@ -103,64 +103,156 @@ namespace cppgear {
         }
     };
 
+    struct PairComparator {
+        template < typename Lhs_, typename Rhs_ >
+        bool operator()(Lhs_ const& lhs, Rhs_ const& rhs) const {
+            return lhs.first == rhs.first && lhs.second == lhs.second;
+        }
+    };
+
     TEST(FlatMapTest, Construction) {
         using Testee = flat_map<std::string, std::string>;
 
         {
             Testee testee;
-            ASSERT_TRUE(testee.empty());
+            EXPECT_TRUE(testee.empty());
         }
         {
             Testee testee((Testee::allocator_type(), Testee::key_compare()));
-            ASSERT_TRUE(testee.empty());
+            EXPECT_TRUE(testee.empty());
         }
         {
             Testee testee((Testee::allocator_type()));
-            ASSERT_TRUE(testee.empty());
+            EXPECT_TRUE(testee.empty());
         }
         {
             Testee testee((Testee::key_compare()));
-            ASSERT_TRUE(testee.empty());
+            EXPECT_TRUE(testee.empty());
+        }
+        {
+            Testee testee{{"one", "jaws"}, {"two", "bite"}, {"three", "claws"}, {"four", "catch"}};
+            EXPECT_EQ(testee.size(), 4);
+            EXPECT_TRUE(std::is_sorted(testee.begin(), testee.end(), testee.value_comp()));
+
+            testee = {{"five", "jaws"}, {"six", "bite"}, {"seven", "claws"}, {"eight", "catch"}};
+            EXPECT_EQ(testee.size(), 4);
+            EXPECT_TRUE(std::is_sorted(testee.begin(), testee.end(), testee.value_comp()));
         }
         {
             using Sample = std::map<std::string, std::string>;
 
             auto sample = Generator<Sample>()();
             Testee testee(sample.begin(), sample.end(), Testee::allocator_type());
-            ASSERT_TRUE(std::is_sorted(testee.begin(), testee.end(), testee.value_comp()));
-            ASSERT_TRUE(std::equal(sample.begin(), sample.end(), testee.begin(), testee.end(),
-                                   [](Sample::value_type const& lhs, Testee::value_type const& rhs) {
-                                        return lhs.first == rhs.first && lhs.second == lhs.second;
-                        }));
+            EXPECT_TRUE(std::is_sorted(testee.begin(), testee.end(), testee.value_comp()));
+            EXPECT_TRUE(std::equal(sample.begin(), sample.end(), testee.begin(), testee.end(), PairComparator()));
         }
         {
             using Sample = std::vector<std::pair<std::string, std::string>>;
 
             auto sample = Generator<Sample>()();
             Testee testee(sample.begin(), sample.end(), Testee::allocator_type());
-            ASSERT_EQ(sample.size(), testee.size());
-            ASSERT_TRUE(std::is_sorted(testee.begin(), testee.end(), testee.value_comp()));
+            EXPECT_EQ(sample.size(), testee.size());
+            EXPECT_TRUE(std::is_sorted(testee.begin(), testee.end(), testee.value_comp()));
 
             Testee testee2(testee);
-            ASSERT_EQ(testee2, testee);
+            EXPECT_EQ(testee2, testee);
 
             Testee testee3(std::move(testee2));
-            ASSERT_EQ(testee3, testee);
+            EXPECT_EQ(testee3, testee);
 
             Testee testee4(testee, Testee::allocator_type());
-            ASSERT_EQ(testee4, testee);
+            EXPECT_EQ(testee4, testee);
 
             Testee testee5(std::move(testee4), Testee::allocator_type());
-            ASSERT_EQ(testee5, testee);
+            EXPECT_EQ(testee5, testee);
 
             Testee testee6;
             testee6 = testee;
-            ASSERT_EQ(testee6, testee);
+            EXPECT_EQ(testee6, testee);
 
             Testee testee7;
             testee7 = std::move(testee6);
-            ASSERT_EQ(testee7, testee);
+            EXPECT_EQ(testee7, testee);
         }
     }
 
+    TEST(FlatMapTest, Lookup) {
+        using Unordered = std::vector<std::pair<std::string, std::string>>;
+        using Sample = std::map<std::string, std::string>;
+        using Testee = flat_map<std::string, std::string>;
+
+        auto unordered = Generator<Unordered>()();
+
+        Sample sample(unordered.begin(), unordered.end());
+        Testee testee(unordered.begin(), unordered.end());
+
+        EXPECT_TRUE(std::is_sorted(testee.begin(), testee.end(), testee.value_comp()));
+        EXPECT_TRUE(std::equal(sample.begin(), sample.end(), testee.begin(), testee.end(), PairComparator()));
+
+        for (auto sample_iter = sample.begin(); sample_iter != sample.end(); ++sample_iter) {
+            auto testee_iter = testee.find(sample_iter->first);
+            ASSERT_NE(testee_iter, testee.end());
+            EXPECT_TRUE(PairComparator()(*testee_iter, *sample_iter));
+        }
+        for (auto testee_iter = testee.begin(); testee_iter != testee.end(); ++testee_iter) {
+            auto sample_iter = sample.find(testee_iter->first);
+            ASSERT_NE(sample_iter, sample.end());
+            EXPECT_TRUE(PairComparator()(*sample_iter, *testee_iter));
+        }
+
+        for (auto sample_iter = sample.rbegin(); sample_iter != sample.rend(); ++sample_iter) {
+            auto testee_iter = testee.find(sample_iter->first);
+            ASSERT_NE(testee_iter, testee.end());
+            EXPECT_TRUE(PairComparator()(*testee_iter, *sample_iter));
+        }
+        for (auto testee_iter = testee.rbegin(); testee_iter != testee.rend(); ++testee_iter) {
+            auto sample_iter = sample.find(testee_iter->first);
+            ASSERT_NE(sample_iter, sample.end());
+            EXPECT_TRUE(PairComparator()(*sample_iter, *testee_iter));
+        }
+
+        for (auto sample_iter = sample.cbegin(); sample_iter != sample.cend(); ++sample_iter) {
+            auto testee_iter = testee.find(sample_iter->first);
+            ASSERT_NE(testee_iter, testee.end());
+            EXPECT_TRUE(PairComparator()(*testee_iter, *sample_iter));
+        }
+        for (auto testee_iter = testee.cbegin(); testee_iter != testee.cend(); ++testee_iter) {
+            auto sample_iter = sample.find(testee_iter->first);
+            ASSERT_NE(sample_iter, sample.end());
+            EXPECT_TRUE(PairComparator()(*sample_iter, *testee_iter));
+        }
+
+        for (auto sample_iter = sample.crbegin(); sample_iter != sample.crend(); ++sample_iter) {
+            auto testee_iter = testee.find(sample_iter->first);
+            ASSERT_NE(testee_iter, testee.end());
+            EXPECT_TRUE(PairComparator()(*testee_iter, *sample_iter));
+        }
+        for (auto testee_iter = testee.crbegin(); testee_iter != testee.crend(); ++testee_iter) {
+            auto sample_iter = sample.find(testee_iter->first);
+            ASSERT_NE(sample_iter, sample.end());
+            EXPECT_TRUE(PairComparator()(*sample_iter, *testee_iter));
+        }
+
+        for (auto& kv : sample) {
+            Testee::mapped_type* value = nullptr;
+            ASSERT_NO_THROW(value = &testee.at(kv.first));
+            EXPECT_EQ(*value, kv.second);
+        }
+        for (auto& kv : testee) {
+            Sample::mapped_type* value = nullptr;
+            ASSERT_NO_THROW(value = &sample.at(kv.first));
+            EXPECT_EQ(*value, kv.second);
+        }
+
+        for (auto& kv : sample) {
+            EXPECT_TRUE(testee.count(kv.first));
+            auto& value = testee[kv.first];
+            EXPECT_EQ(value, kv.second);
+        }
+        for (auto& kv : testee) {
+            EXPECT_TRUE(sample.count(kv.first));
+            auto& value = sample[kv.first];
+            EXPECT_EQ(value, kv.second);
+        }
+    }
 }
