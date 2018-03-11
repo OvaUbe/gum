43a2b5ae99ba1ac4da0a63f6d71b440801f3f2f9
ovaube <trevorismay@gmail.com>
2018-03-12 02:10:43 +0300

    async: Add TaskQueue.

---
+++ gum/async/TaskQueue.cpp
@@ -0,0 +1,53 @@
+/*
+ * Copyright (c) Vladimir Golubev
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+
+#include <gum/async/TaskQueue.h>
+
+#include <gum/functional/Invoker.h>
+#include <gum/Maybe.h>
+#include <gum/Try.h>
+
+namespace gum {
+
+    GUM_DEFINE_LOGGER(TaskQueue);
+
+
+    void TaskQueue::push(Task&& task) {
+        _queue.push_back([task = std::move(task)]{ GUM_TRY_LEVEL("Uncaught exception in queued task", LogLevel::Error, task()); });
+    }
+
+
+    void TaskQueue::run() {
+        while (maybe(pop()).and_(Invoker()));
+    }
+
+
+    Optional<TaskQueue::Task> TaskQueue::pop() {
+        if (_queue.empty())
+            return nullptr;
+
+        Task task = std::move(_queue.front());
+        _queue.pop_front();
+        return task;
+    }
+
+}
