a14e631a88271d3a307b264f734adea3704906de
ovaube <trevorismay@gmail.com>
2018-05-01 03:40:33 +0300

    io: Introduce ISeekable.

---
+++ gum/io/ISeekable.h
@@ -0,0 +1,40 @@
+/*
+ * Copyright (c) Vladimir Golubev
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+
+#pragma once
+
+#include <gum/Enum.h>
+#include <gum/smartpointer/SharedPtr.h>
+#include <gum/smartpointer/SharedReference.h>
+
+namespace gum {
+
+GUM_ENUM(SeekMode, Begin, Current, End);
+
+struct ISeekable {
+    virtual ~ISeekable() {}
+
+    virtual void seek(s64 offset, SeekMode mode = SeekMode::Begin) = 0;
+};
+GUM_DECLARE_PTR(ISeekable);
+GUM_DECLARE_REF(ISeekable);
+}
